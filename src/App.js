ea75b9a14a2cc3a712b032ec4e56d60ba5a66fe1
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-24 21:00:06 -0400

    react app and eslint for coc-vim setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      state: "",
+    };
+  }
+  render() {
+    return <div>Hello from React</div>;
+  }
+}
+
+export default App;
